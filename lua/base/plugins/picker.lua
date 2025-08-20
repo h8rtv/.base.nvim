e350b71315e74923a51703b059391943e886fba2
Heitor <heitorventura21@gmail.com>
2025-08-20 03:47:02 -0300

    Telescope -> Fzf-lua

--- lua/base/plugins/picker.lua
+++ lua/base/plugins/picker.lua
@@ -1,67 +1,29 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  tag = '0.1.8',
-  event = 'VimEnter',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    {
-      'nvim-telescope/telescope-fzf-native.nvim',
-      build = 'make',
-      cond = function()
-        return vim.fn.executable 'make' == 1
-      end,
-    },
-    { 'nvim-telescope/telescope-ui-select.nvim' },
-  },
+  "ibhagwan/fzf-lua",
   config = function()
-    local telescope = require('telescope')
-    local actions = require('telescope.actions')
-    local themes = require('telescope.themes')
-    local builtin = require('telescope.builtin')
-
-    pcall(telescope.load_extension, 'fzf')
-    pcall(telescope.load_extension, 'ui-select')
-
-    telescope.setup {
-      defaults = {
-        mappings = {
-          i = {
-            ['<C-q>'] = actions.smart_send_to_qflist + actions.open_qflist,
-            ['<C-d>'] = actions.delete_buffer,
-          }
-        }
-      },
-      extensions = {
-        ['ui-select'] = themes.get_dropdown
-      },
-    }
+    local fzf = require('fzf-lua')
+    fzf.setup({ 'max-perf' })
 
     local function map(l, r, desc)
       vim.keymap.set('n', l, r, { desc = desc })
     end
-    map('grr', builtin.lsp_references, '[G]oto [R]eferences')
-    map('gri', builtin.lsp_implementations, '[G]oto [I]mplementations')
-    map('grd', builtin.lsp_definitions, '[G]oto [D]efinitions')
-    map('gO', builtin.lsp_document_symbols, 'Open Document Symbols')
-    map('grt', builtin.lsp_type_definitions, '[G]oto [T]ype Definition')
-    map('<leader>sh', builtin.help_tags, '[S]earch [H]elp')
-    map('<leader>sk', builtin.keymaps, '[S]earch [K]eymaps')
-    map('<leader>sf', builtin.find_files, '[S]earch [F]iles')
-    map('<leader>sw', builtin.grep_string, '[S]earch current [W]ord')
-    map('<leader>sg', builtin.live_grep, '[S]earch by [G]rep')
-    map('<leader>sd', builtin.diagnostics, '[S]earch [D]iagnostics')
-    map('<leader>sr', builtin.resume, '[S]earch [R]esume')
-    map('<leader>s.', builtin.oldfiles, '[S]earch Recent Files ("." for repeat)')
-    map('<leader><leader>', builtin.buffers, '[ ] Find existing buffers')
-    local function curr_buf_fuzzy_find()
-      builtin.current_buffer_fuzzy_find(themes.get_dropdown {
-        winblend = 10,
-        previewer = false,
-      })
-    end
-    map('<leader>/', curr_buf_fuzzy_find, '[/] Fuzzily search in current buffer')
+    map('grr', fzf.lsp_references, '[G]oto [R]eferences')
+    map('gri', fzf.lsp_implementations, '[G]oto [I]mplementations')
+    map('grd', fzf.lsp_definitions, '[G]oto [D]efinitions')
+    map('gO', fzf.lsp_document_symbols, 'Open Document Symbols')
+    map('grt', fzf.lsp_typedefs, '[G]oto [T]ype Definition')
+    map('<leader>sh', fzf.help_tags, '[S]earch [H]elp')
+    map('<leader>sk', fzf.keymaps, '[S]earch [K]eymaps')
+    map('<leader>sf', fzf.files, '[S]earch [F]iles')
+    map('<leader>sw', fzf.grep_cword, '[S]earch current [W]ord')
+    map('<leader>sg', fzf.live_grep_native, '[S]earch by [G]rep')
+    map('<leader>sd', fzf.diagnostics_workspace, '[S]earch [D]iagnostics')
+    map('<leader>sr', fzf.resume, '[S]earch [R]esume')
+    map('<leader>s.', fzf.oldfiles, '[S]earch Recent Files ("." for repeat)')
+    map('<leader><leader>', fzf.buffers, '[ ] Find existing buffers')
+    map('<leader>/', fzf.lgrep_curbuf, '[/] Fuzzily search in current buffer')
     local function find_config_files()
-      builtin.find_files { cwd = vim.fn.stdpath 'config' }
+      fzf.files { cwd = vim.fn.stdpath 'config' }
     end
     map('<leader>sn', find_config_files, '[S]earch [N]eovim files')
   end,
